846c3751d66d3a93fe5c6c26c306b62547019b26
Jan Varhol <jan@varhol.sk>
2019-01-12 23:00:48 +0100

    Add agpl license information

--- lib/lib.sh
+++ lib/lib.sh
@@ -1,8 +1,18 @@
-### This is standard library working example
+# This file is part of Broadexec.
 #
-# B=basic(list of numbers of helper or other functions used)
-# H=helper(list of numbers of functions used in)
-# OS=os specific
+# Broadexec is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Broadexec is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Broadexec.  If not, see <https://www.gnu.org/licenses/>.
+
 #
 #######################
 #######################
