669c3f3eda5f5dfcf25316a6a3175af68d13ed58
Jan Varhol <jan@varhol.sk>
2019-10-28 20:34:29 +0100

    Add testing library features   - selftesting before allowing to git commit only if all tests pass   - add possibility to filter test case output (eg. lines with ID's)   - add new test cases   - fix getopts error when running testing

--- lib/testing_lib.sh
+++ lib/testing_lib.sh
@@ -33,20 +33,15 @@ testing_init_checks () {
 
 testing_load_scenario () {
 
-  #testing_checkout_branch teamconfigs testing_suite
+  if [ ! -z "${BRDEXEC_TESTING_GITHOOK}" ]; then
+    echo "BRDEXEC SELFTEST: Running $(cat "${TESTING_SCENARIO_FILE}" | grep -c "^scenario ") test cases."
+  fi
 
   ### check if list/scenario files are existing
   if [ ! -f "${TESTING_SCENARIO_FILE}" ]; then
     echo "Scenario or list provided does not exists"
     exit 1
   fi
-
-  ### for now only providing lists works
-  #echo -e "\nLoading scenario list"
-  #if [ "$(echo "${TESTING_SCENARIO_FILE}" | grep -c ".list$")" -ne 1 ]; then
-  #  echo "List provided seems not be proper list. File needs to be ending with .list. Name of your file: ${TESTING_SCENARIO_FILE}"
-  #  exit 1
-  #fi
 
   ### displaying scenario list message
   TESTING_SCENARIO_LIST_MESSAGE="$(cat "${TESTING_SCENARIO_FILE}" | grep "^message " | tail -n 1 | cut -d " " -f2-)"
@@ -73,12 +68,16 @@ testing_load_scenario () {
         echo "Scenario file ${TESTING_SCENARIO_FILE} is invalid"
         TESTING_SCENARIO_FILE_ERROR="YES"
       else
-        echo -e "Scenario file ${TESTING_SCENARIO_FILE} is \e[32mOK\e[0m"
+        if [ -z "${BRDEXEC_TESTING_GITHOOK}" ]; then
+          echo -e "Scenario file ${TESTING_SCENARIO_FILE} is \e[32mOK\e[0m"
+        fi
       fi
     fi
   done
   if [ -z "${TESTING_SCENARIO_FILE_ERROR}" ] && [ ! -z "${TESTING_SCENARIO_LIST}" ]; then
-    echo -e "ALL scenario files are \e[32mOK\e[0m"
+    if [ -z "${BRDEXEC_TESTING_GITHOOK}" ]; then
+      echo -e "ALL scenario files are \e[32mOK\e[0m"
+    fi
   else
     echo "There is some issues with scenario files, please resolve them and run session again."
     exit 1
@@ -92,12 +91,6 @@ testing_execute_scenarios () {
   fi
 
   for TESTING_SCENARIO_FILE in ${TESTING_SCENARIO_LIST}; do
-
-    ### display 4 horizontal lines between testing scenarios
-    #echo -e "\n"
-    #for whatever in 1 7 9 2; do #like for real look up that number, it is interesting
-    #  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-    #done
 
     ### load scenario data
     TESTING_SCENARIO_FILE_MESSAGE="$(cat "${TESTING_SCENARIO_FILE}" | grep "^message " | tail -n 1 | cut -d " " -f2-)"
@@ -114,56 +107,79 @@ testing_execute_scenarios () {
         sed '/^expect_start/,$!d' ${TESTING_SCENARIO_FILE} | sed '/^expect_end/q' >> ${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE_TEMP}
         sed -i '1d; $d' ${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE_TEMP}
 
-        for BRDEXEC_HOST_ITEM in $(seq 10); do
-          if [ ! -z "${brdexec_test_host[$BRDEXEC_HOST_ITEM]}" ]; then
-            sed -i "s/^host${BRDEXEC_HOST_ITEM}/${brdexec_test_host[$BRDEXEC_HOST_ITEM]}/g" ${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE_TEMP}
-          fi
-        done
-
         cat ${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE_TEMP} | sort -rn > ${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE}
         rm ${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE_TEMP}
+        testing_filter_host "${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE}"
       fi
     fi
 
-    ### display scenario message
-    #if [ "${TESTING_SCENARIO_FILE_MESSAGE}" = "" 2>/dev/null ]; then
-    #  echo -e "\nNo scenario file message found. Consider adding it for convenience of your colleagues."
-    #else
     #  echo -e "\nScenario ${TESTING_SCENARIO_FILE} message:\n${TESTING_SCENARIO_FILE_MESSAGE}\n"
-    #fi
-    echo -e "\nExecuting ${TESTING_SCENARIO_FILE}\n./broadexec.sh ${TESTING_SCENARIO_PARAMETERS}"
-    #echo "Press ENTER to continue..."
-    #read
+    if [ -z "${BRDEXEC_TESTING_GITHOOK}" ]; then
+      echo -e "\nExecuting ${TESTING_SCENARIO_FILE}\n./broadexec.sh ${TESTING_SCENARIO_PARAMETERS}"
+    fi
 
     ### run scenario
     if [ "${TESTING_SCENARIO_PARAMETERS}" = "" 2>/dev/null ]; then
       echo "Failed to obtain parameters for broadexec run, please check manually."
+      if [ ! -z "${BRDEXEC_TESTING_GITHOOK}" ]; then
+        exit 1
+      fi
     else
       if [ "${TESTING_SCENARIO_EXPECT_AUTO_RESULTS}" = "yes" ]; then
         TESTING_SCENARIO_REAL_AUTO_RESULTS_FILE="$(mktemp /tmp/broadexec_testing_lib.XXXXXXXXXX)"
-        ./broadexec.sh ${TESTING_SCENARIO_PARAMETERS} | sort -rn > ${TESTING_SCENARIO_REAL_AUTO_RESULTS_FILE}
+        TESTING_SCENARIO_TEMP_FILE="$(mktemp /tmp/broadexec_testing_lib.XXXXXXXXXX)"
+        ./broadexec.sh ${TESTING_SCENARIO_PARAMETERS} > ${TESTING_SCENARIO_TEMP_FILE} 2>&1
+        cat $TESTING_SCENARIO_TEMP_FILE | sort -rn > ${TESTING_SCENARIO_REAL_AUTO_RESULTS_FILE}
       else
         ./broadexec.sh ${TESTING_SCENARIO_PARAMETERS}
       fi
     fi
 
+    testing_filter_host "${TESTING_SCENARIO_REAL_AUTO_RESULTS_FILE}"
+
+    TESTING_SCENARIO_TEMP_EXCLUDE_FILE="$(mktemp /tmp/broadexec_testing_lib.XXXXXXXXXX)"
+    while read TESTING_EXCLUDE_LINE; do
+      if [ "$(echo "${TESTING_EXCLUDE_LINE}" | grep -c "^exclude_line ")" -eq 1 ]; then
+        cp -p "${TESTING_SCENARIO_REAL_AUTO_RESULTS_FILE}" $TESTING_SCENARIO_TEMP_EXCLUDE_FILE
+        cat $TESTING_SCENARIO_TEMP_EXCLUDE_FILE | grep -v "$(echo "${TESTING_EXCLUDE_LINE}"  | cut -d " " -f2- )" > "${TESTING_SCENARIO_REAL_AUTO_RESULTS_FILE}"
+      fi
+    done < "${TESTING_SCENARIO_FILE}"
+    rm $TESTING_SCENARIO_TEMP_EXCLUDE_FILE
+
     ### check auto results
     if [ "${TESTING_SCENARIO_EXPECT_AUTO_RESULTS}" = "yes" ]; then
       if [ "$(diff ${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE} ${TESTING_SCENARIO_REAL_AUTO_RESULTS_FILE} | wc -l)" -eq 0 ]; then
-        echo -e "\e[32mTEST SUCCESSFULL\e[0m"
-#diff ${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE} ${TESTING_SCENARIO_REAL_AUTO_RESULTS_FILE}
-#echo "RETURN $?"
-#
-#echo
-#echo "${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE}"
-#cat ${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE}
-#echo "${TESTING_SCENARIO_REAL_AUTO_RESULTS_FILE}"
-#cat ${TESTING_SCENARIO_REAL_AUTO_RESULTS_FILE}
+        if [ ! -z "${BRDEXEC_TESTING_GITHOOK}" ]; then
+          echo -e "$(echo "${TESTING_SCENARIO_FILE}" | awk -F "/" '{print $4}' | awk -F "_" '{print $1}'): \e[32mOK\e[0m; \c"
+        else
+          echo -e "\e[32mTEST SUCCESSFULL\e[0m"
+        fi
+
       else
-        echo -e "\n\e[91mERROR:\e[0m There are differences between expectation and real results. \e[91mCHECK!\e[0m\n"
-        diff ${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE} ${TESTING_SCENARIO_REAL_AUTO_RESULTS_FILE}
+        if [ ! -z "${BRDEXEC_TESTING_GITHOOK}" ]; then
+          echo -e "$(echo "${TESTING_SCENARIO_FILE}" | awk -F "/" '{print $4}' | awk -F "_" '{print $1}'): \e[91mFAIL\e[0m; \c"
+          BRDEXEC_TESTING_GITHOOK_ERROR=true
+        else
+          echo -e "\n\e[91mERROR:\e[0m There are differences between expectation and real results. \e[91mCHECK!\e[0m\n"
+          diff ${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE} ${TESTING_SCENARIO_REAL_AUTO_RESULTS_FILE}
+        fi
       fi
       rm ${TESTING_SCENARIO_EXPECT_AUTO_RESULTS_FILE} ${TESTING_SCENARIO_REAL_AUTO_RESULTS_FILE}
+    fi
+  done
+  if [ ! -z "${BRDEXEC_TESTING_GITHOOK}" ]; then
+    echo
+    if [ ! -z "${BRDEXEC_TESTING_GITHOOK_ERROR}" ]; then
+      exit 1 # tell git not to commit if any of the tests fail
+    fi
+  fi
+}
+
+testing_filter_host () {
+
+  for BRDEXEC_HOST_ITEM in $(seq 10); do
+    if [ ! -z "${BRDEXEC_TEST_HOST[$BRDEXEC_HOST_ITEM]}" ]; then
+      sed -i "s/brdexec_test_host${BRDEXEC_HOST_ITEM}/${BRDEXEC_TEST_HOST[$BRDEXEC_HOST_ITEM]}/g" "${1}"
     fi
   done
 }
